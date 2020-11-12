a6cc9f2b57d9c512168241fe196c8209f9a2b302
mansib1992 <66750821+mansib1992@users.noreply.github.com>
2020-11-12 15:39:21 +0600

    null vs undifine battle

---
+++ null-vs-undefine.js
@@ -0,0 +1,18 @@
+let pakhi;
+
+function add(num1, num2){
+    console.log(num1 + num2); 
+    return  
+}   
+
+function add(num1, num2){
+    console.log(num1, num2); 
+
+let fun = undefined;
+console.log(fun);
+
+const premik = {name:"smart dude", phone:"45478"}
+console.log(premik.gf);
+
+const result = add(45);
+console.log(result);
--- true-false.js
+++ true-false.js
@@ -1,14 +1,15 @@
 // Falsy;
+// false
 // 0
 // ""
 // undefined
 // null
 // NaN
 // truthy
-// '0' ''
-let name = [];
+// '0' '' [] {}
+let name = 12;
 console.log(name);
-if(name){
+if(name || name ==0){
     console.log("condition is true");
 }
 else{
